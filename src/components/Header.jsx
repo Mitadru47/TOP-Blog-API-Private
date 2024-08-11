b16b2ed33f437f98cd23309ecf12c846dd44c093
Mitadru <Mitadru.Gangopadhyay@aot.edu.in>
2024-08-11 20:51:47 +0530

    Project Refactoring & Restructuring

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Header(){
+
+    return(
+
+        <div id="top-nav">
+
+            <div id="header-container">
+                <a id="header" href="/">Blog API</a>
+            </div>
+            
+        </div>
+    );
+}
+
+export default Header;
