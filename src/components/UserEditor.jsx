0d333b7a5516c2780ae1348ad39a394aaa741753
Mitadru <Mitadru.Gangopadhyay@aot.edu.in>
2024-09-06 23:00:14 +0530

    Revamped UserEditor Form Submission Logic

--- src/components/UserEditor.jsx
+++ src/components/UserEditor.jsx
@@ -21,7 +21,50 @@ async function getUser(setUserResponse){
         .catch((error) => { console.log(error); })
 }
 
-function UserEditor(props){
+function handleSubmit(event){
+
+    event.preventDefault();
+
+    // Form Data Parsing
+
+    const data = new FormData(event.currentTarget);
+
+    const plainFormData = Object.fromEntries(data.entries());
+	const formDataJsonString = JSON.stringify(plainFormData);
+
+    // API Header Creation
+
+    const headers = new Headers();
+
+    headers.append('Content-Type', 'application/json');
+    headers.append('Authorization', localStorage.getItem("token"));
+
+    if(isLoggedIn()){
+
+        fetch("http://localhost:3000/dashboard/user/edit", { 
+            
+                mode: "cors", 
+                method: "POST", 
+                
+                headers: headers,
+                body: formDataJsonString 
+            })
+
+            .then((response) => response.json())
+            .then((responseBody) => {
+            
+                if(responseBody === "Success!")
+                    window.location.href = "http://localhost:5174/dashboard/user";
+            })
+
+            .catch((error) => console.log(error));
+    }
+
+    else
+        window.location.href = "http://localhost:5174/dashboard";
+}
+
+function UserEditor(){
 
     if(isLoggedIn()){
 
@@ -40,7 +83,7 @@ function UserEditor(props){
 
                         <div id="form-container">
 
-                            <form action="http://localhost:3000/dashboard/user/edit" method="POST">
+                            <form onSubmit={handleSubmit}>
 
                                 <input type="text" name="firstName" id="firstname" defaultValue={userResponse[0].firstName}/>
                                 <input type="text" name="lastName" id="lastname" defaultValue={userResponse[0].lastName}/>
@@ -57,7 +100,7 @@ function UserEditor(props){
 
                                 <input type="text" name="id" id="id" defaultValue={userResponse[0].id} hidden/>
                                 
-                                <button type="submit" id="submit-button">Update User Details</button>
+                                <button type="submit" id="submit-button">Update Author Details</button>
                                 <a id="cancel-button" href={"/dashboard" + userResponse[0].url}>Cancel</a>
 
                             </form>
